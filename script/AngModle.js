a82a609ab093a70dc62091acb624230e45ac475a
unknown <harish.kumar@aspiringminds.in>
2015-10-19 13:14:56 +0530

    angular lession 1

---
+++ script/AngModle.js
@@ -0,0 +1,7 @@
+
+var myModule= angular.module('myMoudule', []);
+myModule.controller('SimpleController', function($scope){
+    
+    $scope.cust = [{name: 'harish' , last :'kumar'},{name: 'vikesh' , last :'prajapat'},{name: 'pardeep' , last :'varma'} ];
+ 
+})
